ee7d3dd1b010293af7295b95debaf4aff002d0a7
Gabriel <gszcode@gmail.com>
2022-11-15 16:22:09 -0300

    dashboard page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { DefaultLayout } from './layout/DefaultLayout'
 import { Entry } from './page/entry/Entry.page'
 import './App.css'
+import { Dashboard } from './page/dashboard/Dashboard.page'
 
 function App() {
   return (
     <div className="App">
       {/* <Entry /> */}
-      <DefaultLayout>// Dashboard</DefaultLayout>
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
     </div>
   )
 }
